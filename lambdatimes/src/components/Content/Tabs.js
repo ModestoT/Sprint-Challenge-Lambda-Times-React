8a3353447fe0d8ce6efde7cf59935bcf52c708a0
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-18 11:24:17 -0600

    Removed comments from completed components and added Proptype checks to the tabs component

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import Tab from './Tab';
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {props.tabs.map((tab) => {
-          return <Tab tab={tab} selectedTab={props.selected} selectTabHandler={props.selectTabHandler}/>
+        {props.tabs.map((tab, index) => {
+          return <Tab tab={tab} key={index} selectedTab={props.selected} selectTabHandler={props.selectTabHandler}/>
         })}
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
     </div>
   );
 };
+
+Tabs.propTypes = {
+  tabs: PropTypes.array.isRequired,
+  selectedTab: PropTypes.string,
+  selectTabHandler:  PropTypes.func
+}
 
 // Make sure to use PropTypes to validate your types!
 export default Tabs;
