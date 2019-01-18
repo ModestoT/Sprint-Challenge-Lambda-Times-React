f80a477cb69265aa786c55a138416aa16cfc0577
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-18 12:33:43 -0600

    Completed the Carousel component. You can now scroll infinitly to the left or right

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,33 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { carouselData } from '../../data'
 // Complete this Carousel 
-export default class Carousel extends Component {
+export default class Carousel extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      imgs: [],
+      index: 0,
+      counter: 0,
     }
   }
   componentDidMount(){
-
+    this.setState({imgs: carouselData});
   }
 
   leftClick = () => {
+    let counter = this.state.counter -1;
+    let length = this.state.imgs.length;
 
+    if(counter < 0) {
+      counter=length -1
+    } 
+    this.setState({index: counter, counter: counter})
   }
 
   rightClick = () => {
+    let counter = this.state.counter +1;
 
+    if(!this.state.imgs[counter] ) {
+      counter = 0;
+    }
+    this.setState({index: counter, counter:counter});
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={this.state.imgs[this.state.index]} style={{display: 'block'}} />
   }
   
   render(){
+    // console.log(`counter: ${this.state.counter}`);
+    // console.log(`index: ${this.state.index}`);
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {this.selectedImage()}
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
